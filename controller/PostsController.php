791515f5b2bd14e7691891debfc8f0b92a3690fc
Aurélien <aurel.yenyen70@gmail.com>
2023-02-03 14:10:49 +0100

    Répare bug affichage

--- controller/PostsController.php
+++ controller/PostsController.php
@@ -6,20 +6,16 @@ class PostsController extends Controller{
 
     function index(){
         $this->loadModel('Post');
-        $condition = array('online' => 1, 'type' => 'post'); // Définit la condition de la requête MySQL
         $d['posts'] = $this->Post->find(array(
             'conditions' => array('online' => 1, 'type' => 'post' )
         ));
-        $d['total'] = $this->Post->findCount($condition);
-
         $this->set($d);
     }
     
     function view($id){
         $this->loadModel('Post');
-        $condition = array('online' => 1,'id'=>$id, 'type' => 'post'); // Définit la condition de la requête MySQL
         $d['page'] = $this->Post->findFirst(array(
-            'conditions'
+            'conditions' => array('online' => 1,'id'=>$id, 'type' => 'post') // Définit la condition de la requête MySQL
         ));
         if(empty($d['page'])){
             $this->e404('Page introuvable');
---
+++ view/pages/view.php
@@ -0,0 +1,8 @@
+<?php
+
+    $title_for_layout = $page->name;
+
+?>
+
+<h1><?php echo $page->name;?></h1>
+<h3><?php echo $page->content;?></h1>
