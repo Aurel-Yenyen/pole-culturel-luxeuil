829cc2b439c781ee4e8a855a93e82b77b6c452a1
Aurélien <aurel-yenyen@outlook.fr>
2023-03-19 17:13:55 +0100

    Ajout de la date, voir pr ajout image, stylisation de l'espace connetction...

--- controller/BackofficeController.php
+++ controller/BackofficeController.php
@@ -78,6 +78,89 @@ class BackofficeController extends Controller{
             'conditions' => $conditions10
         ));
         $this->set($d);
+        $this->set($d);
+        // On vérifie si le formulaire a été soumis
+        if(isset($_POST['submit'])) {
+            // On récupère les valeurs des champs
+            $colorPrimary1 = $_POST['colorPrimary1'];
+            $colorPrimary2 = $_POST['colorPrimary2'];
+            $colorPrimary3 = $_POST['colorPrimary3'];
+            $colorPrimary4 = $_POST['colorPrimary4'];
+            $colorPrimary5 = $_POST['colorPrimary5'];
+            $colorPrimary6 = $_POST['colorPrimary6'];
+            $colorPrimary7 = $_POST['colorPrimary7'];
+            $colorPrimary8 = $_POST['colorPrimary8'];
+            $colorPrimary9 = $_POST['colorPrimary9'];
+            $colorPrimary10 = $_POST['colorPrimary10'];
+            $colorPrimary11 = $_POST['colorPrimary11'];
+            $colorPrimary12 = $_POST['colorPrimary12'];
+            $colorPrimary13 = $_POST['colorPrimary13'];
+            $colorPrimary14 = $_POST['colorPrimary14'];
+            $colorPrimary15 = $_POST['colorPrimary15'];
+            $colorPrimary16 = $_POST['colorPrimary16'];
+            $colorPrimary17 = $_POST['colorPrimary17'];
+            $colorPrimary18 = $_POST['colorPrimary18'];
+            $colorPrimary19 = $_POST['colorPrimary19'];
+            $colorPrimary20 = $_POST['colorPrimary20'];
+            $colorPrimary21 = $_POST['colorPrimary21'];
+            $colorPrimary22 = $_POST['colorPrimary22'];
+            $colorPrimary23 = $_POST['colorPrimary23'];
+
+
+
+
+
+            // On construit le contenu CSS avec les nouvelles valeurs
+            $content = <<<CSS
+:root {
+    --colorPrimary-1: $colorPrimary1;
+    --colorPrimary-2: $colorPrimary2;
+    --colorPrimary-3: $colorPrimary3;
+    --colorPrimary-4: $colorPrimary4;
+    --colorPrimary-5: $colorPrimary5;
+    --colorPrimary-6: $colorPrimary6;
+    --colorPrimary-7: $colorPrimary7;
+    --colorPrimary-8: $colorPrimary8;
+    --colorPrimary-9: $colorPrimary9;
+    --colorPrimary-10: $colorPrimary10;
+    --colorPrimary-11: $colorPrimary11;
+    --colorPrimary-12: $colorPrimary12;
+    --colorPrimary-13: $colorPrimary13;
+    --colorPrimary-14: $colorPrimary14;
+    --colorPrimary-15: $colorPrimary15;
+    --colorPrimary-16: $colorPrimary16;
+    --colorPrimary-17: $colorPrimary17;
+    --colorPrimary-18: $colorPrimary18;
+    --colorPrimary-19: $colorPrimary19;
+    --colorPrimary-20: $colorPrimary20;
+    --colorPrimary-21: $colorPrimary21;
+    --colorPrimary-22: $colorPrimary22;
+    --colorPrimary-23: $colorPrimary23;
+
+
+
+
+}
+CSS;
+
+            // On écrit le contenu dans le fichier CSS
+            $file = fopen(ROOT . '/webroot/css/root/root.css', 'w');
+            fwrite($file, $content);
+            fclose($file);
+        }
+
+        // On récupère les valeurs actuelles des couleurs
+        $rootContent = file_get_contents(ROOT . '/webroot/css/root/root.css');
+        preg_match_all('/--colorPrimary-\d+:\s(.+);/', $rootContent, $matches);
+        $colors = array_combine(
+            range(1, count($matches[1])),
+            $matches[1]
+        );
+
+        // On charge la vue d'administration avec les couleurs actuelles
+        $this->render('admin_index', [
+            'colors' => $colors
+        ]);
     }
 
 
--- view/pages/index.php
+++ view/pages/index.php
@@ -42,16 +42,22 @@
             <div class="content-title">
                 <h3 class="title-affiche">Les Évènements à venir</h1>
             </div>
-            <?php $posts = $this->loadModel('Post');?>
-<?php print_r($posts)?>
-    <?php foreach ($posts as $p): ?>
-        <li>
-            <h3><?php echo $p->name; ?></h3>
-            <p>Date: <?php echo $p->date; ?></p>
-            <p>Description: <?php echo $p->content; ?></p>
-        </li>
-    <?php endforeach; ?>
+            <?php // Connexion à la base de données
+$db = new PDO('mysql:host=localhost;dbname=poleculturel', 'root', '');
 
+// Préparation de la requête SQL
+$sql = "SELECT * FROM posts WHERE date > NOW() ORDER BY date ASC LIMIT 3";
+
+// Exécution de la requête SQL
+$stmt = $db->query($sql);
+
+// Récupération des résultats
+$posts = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+// Affichage des résultats
+foreach ($posts as $post) {
+    echo $post['name'] . ' - ' . $post['date'] . '<br>';
+}?>
 
             
             <div class="cards" id="cards">
